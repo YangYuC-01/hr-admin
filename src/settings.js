ae471d7ebcfa2d76e229affa9a97e16c35cedb93
YangYuC-01 <1941336678@qq.com>
2022-07-13 11:50:11 +0800

    '项目初始化'

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+  title: '人力资源管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   * 设置 appMain 右侧主体上面的header 是否固定定位
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   * 设置侧边栏 logo是否显示
+   */
+  sidebarLogo: true
+}
